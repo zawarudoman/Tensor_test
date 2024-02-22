1b05b6969538659213aa1ab3d2a52dc0822a3a92
Максим Лесков <max.leskoff@icloud.com>
2024-02-22 20:02:26 +0300

    test commit

--- task1.py
+++ task1.py
@@ -1,35 +1,49 @@
-import pytest
+import logging
 
+import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from seleniumwire import webdriver
+
+from pages.ContactPage import ContactPage
+from pages.SbisPage import SbisPage
+from pages.TensorPage import TensorPage
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture()
-def browser():
+def driver():
     chrome_browser = webdriver.Chrome()
     chrome_browser.implicitly_wait(10)
     return chrome_browser
 
 
-def test_follow_link_tensor_about(browser):
-    browser.get("https://sbis.ru/")
-    menu = browser.find_elements(By.CLASS_NAME, 'sbisru-Header__menu-link')
-    menu[1].click()
-    tensor = browser.find_element(By.CLASS_NAME, 'sbisru-Contacts__logo-tensor')
-    tensor.click()
-    browser.switch_to.window(browser.window_handles[1])
-    find_forse_people = browser.find_elements(By.CLASS_NAME, 'tensor_ru-Index__card-title')
-    assert find_forse_people[1].is_displayed()
-    assert find_forse_people[1].text == 'Сила в людях'
-    find_forse_people = browser.find_elements(By.CSS_SELECTOR, 'a[class="tensor_ru-link tensor_ru-Index__link"]')
-    browser.execute_script('arguments[0].click()', find_forse_people[1])
-    assert 'https://tensor.ru/about' == browser.current_url
-    photos = browser.find_elements(By.CSS_SELECTOR, 'div[class="tensor_ru-About__block3-image-wrapper"]')
-    first_photo = photos[0]
-    first_photo_height = first_photo.size['height']
-    first_photo_width = first_photo.size['width']
-    for photo in photos:
-        assert photo.size['height'] == first_photo_height
-        assert photo.size['width'] == first_photo_width
-    browser.quit()
+def test_follow_link_tensor_about(driver):
+    sbis_page = SbisPage(driver)
+    contact_page = ContactPage(driver)
+    tensor_page = TensorPage(driver)
+    try:
+        logger.debug('Начало теста')
+        sbis_page.open()
+        sbis_page.click_contact()
+        contact_page.find_element_tensor_link()
+        contact_page.click_on_element()
+        driver.switch_to.window(driver.window_handles[1])
+        logger.info(f'Переход на новую вкладку {driver.current_url}')
+        forse_people = tensor_page.find_force_people()
+        assert forse_people.is_displayed()
+        assert forse_people.text == 'Сила в людях'
+        find_about_people = tensor_page.find_about_people()
+        driver.execute_script('arguments[0].click()', find_about_people[1])
+        assert 'https://tensor.ru/about' == driver.current_url
+        photos = tensor_page.find_photos()
+        logger.info(f'Получен список фотографий: {photos}')
+        first_photo = photos[0]
+        first_photo_height = first_photo.size['height']
+        first_photo_width = first_photo.size['width']
+        for photo in photos:
+            assert photo.size['height'] == first_photo_height
+            assert photo.size['width'] == first_photo_width
+    finally:
+        logger.debug("Конец теста")
+        driver.quit()
